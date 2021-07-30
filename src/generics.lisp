0749a753fbd5b4d360c8179e6012b88fa5c28c67
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-07-30 16:12:54 +0300

    generics: commit PARSE-OPTION generic function

--- src/generics.lisp
+++ src/generics.lisp
@@ -3,7 +3,8 @@
   (:use :cl)
   (:export
    :find-short-option
-   :find-long-option))
+   :find-long-option
+   :parse-option))
 (in-package :clingon.generics)
 
 (defgeneric find-short-option (object name &key)
@@ -11,3 +12,6 @@
 
 (defgeneric find-long-option (object name &key)
   (:documentation "Returns the long option with the given NAME, or NIL otherwise"))
+
+(defgeneric parse-option (kind object &key)
+  (:documentation "Parses an option of the given KIND"))
