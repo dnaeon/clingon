d75570813b5bd32e896903e5693e084ed92d8007
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-08-04 11:32:35 +0300

    utils: initial commit of the CLINGON.UTILS package
    Add the CLINGON.UTILS:WALK function for traversing a tree structure in
    DFS and BFS order.

---
+++ src/utils.lisp
@@ -0,0 +1,30 @@
+(in-package :cl-user)
+(defpackage :clingon.utils
+  (:use :cl)
+  (:export
+   :walk))
+(in-package :clingon.utils)
+
+(defun walk (root neighbors-func &key (order :dfs))
+  "Walks a tree structure starting from ROOT. Neighbors of each node are
+  discovered by invoking the NEIGHBORS-FUNC function, which should accept a
+  single argument -- the node we are currently visiting, and should
+  return a list of adjacent nodes.
+
+  The ORDER should be either :dfs or :bfs for Depth-First Search or
+  Breadth-First Search respectively."
+  (let ((to-visit (list root))
+        (visited nil))
+    (loop :while to-visit
+          :for node = (pop to-visit)
+          :for neighbors = (funcall neighbors-func node)
+          :for not-seen = (remove-if (lambda (x)
+                                       (member x visited :test #'equal))
+                                     neighbors)
+          :do
+             (ecase order
+               (:bfs (setf to-visit (nconc to-visit not-seen)))
+               (:dfs (setf to-visit (nconc not-seen to-visit))))
+             (unless (member node visited :test #'equal)
+               (push node visited)))
+    (nreverse visited)))
