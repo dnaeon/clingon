4356ba959da3bee54a7fcfb82c6f34e1bd269d74
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-08-08 14:01:22 +0300

    tests: add tests for generic boolean options

--- tests/test-options.lisp
+++ tests/test-options.lisp
@@ -73,3 +73,22 @@
     (ng (clingon:end-of-options-p "-") "`-' does not match end of options")
     (ok (clingon:end-of-options-p "--") "`--' matches end of options")
     (ng (clingon:end-of-options-p "---") "`---' does not match end of options")))
+
+(deftest option-booleans
+  (testing "generic boolean"
+    (let ((foo (clingon:make-option :boolean
+				    :help "foo boolean"
+				    :short-name #\b
+				    :key :boolean)))
+      (clingon:initialize-option foo)
+      (ok (equal :true (clingon:option-value foo)) "option is properly initialized")
+      (ok (equal :true (clingon:derive-option-value foo "true")) "derive value from \"true\"")
+      (ok (equal :true (clingon:derive-option-value foo "1")) "derive value from \"1\"")
+      (ok (equal :false (clingon:derive-option-value foo "false")) "derive value from \"false\"")
+      (ok (equal :false (clingon:derive-option-value foo "0")) "derive value from \"0\"")
+      (ok (equal :false (clingon:derive-option-value foo nil)) "derive value from nil")
+      (ok (equal :false (clingon:derive-option-value foo "random-string")) "derive value from \"random-string\"")
+
+      ;; value place has not been set, so it defaults to :true
+      (ok (equal t (clingon:finalize-option foo)) "finalized value matches"))))
+
