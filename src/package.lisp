aa2f23634e3d165242fc1e45434bd8cc657bf7ed
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-08-09 13:21:29 +0300

    package: re-export symbols from clingon packages

--- src/package.lisp
+++ src/package.lisp
@@ -2,3 +2,9 @@
 (defpackage :clingon
   (:use :cl))
 (in-package :clingon)
+
+(cl-reexport:reexport-from :clingon.utils)
+(cl-reexport:reexport-from :clingon.conditions)
+(cl-reexport:reexport-from :clingon.generics)
+(cl-reexport:reexport-from :clingon.options)
+(cl-reexport:reexport-from :clingon.command)
