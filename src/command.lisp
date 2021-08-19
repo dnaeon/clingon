85baf34d2da29057d59d6fd0485deedbc1b91c59
Marin Atanasov Nikolov <dnaeon@gmail.com>
2021-08-19 10:34:52 +0300

    command: commit an implementation of PRINT-DOCUMENTATION for Markdown format

--- src/command.lisp
+++ src/command.lisp
@@ -111,6 +111,9 @@
    :print-bash-completions
    :print-bash-completions-and-exit
    :print-documentation
+   :print-options-usage
+   :print-sub-commands-info
+   :command-usage-string
    :visible-options))
 (in-package :clingon.command)
 
@@ -134,30 +137,30 @@
 
 (defparameter *default-help-flag*
   (make-option :flag
-	       :description "display usage information and exit"
-	       :long-name "help"
-	       :key :clingon.help.flag)
+               :description "display usage information and exit"
+               :long-name "help"
+               :key :clingon.help.flag)
   "The default `--help' flag")
 
 (defparameter *default-version-flag*
   (make-option :flag
-	       :description "display version and exit"
-	       :long-name "version"
-	       :key :clingon.version.flag)
+               :description "display version and exit"
+               :long-name "version"
+               :key :clingon.version.flag)
   "The default `--version' flag")
 
 (defparameter *default-bash-completions-flag*
   (make-option :flag
-	       :hidden t
-	       :description "generate bash completions"
-	       :long-name "bash-completions"
-	       :key :clingon.bash-completions.flag)
+               :hidden t
+               :description "generate bash completions"
+               :long-name "bash-completions"
+               :key :clingon.bash-completions.flag)
   "The default `--bash-completions' flag")
 
 (defparameter *default-options*
   (list *default-help-flag*
-	*default-version-flag*
-	*default-bash-completions-flag*)
+        *default-version-flag*
+        *default-bash-completions-flag*)
   "A list of default options to add to each sub-command")
 
 (defclass command ()
@@ -299,13 +302,13 @@
   (let ((required-options (remove-if-not #'option-required-p (command-options command))))
     (dolist (option required-options)
       (unless (option-is-set-p option)
-	(error 'missing-required-option-value :item option :command command)))))
+        (error 'missing-required-option-value :item option :command command)))))
 
 (defmethod find-option ((kind (eql :short)) (command command) name &key)
   "Finds the option with the given short name"
   (find name (command-options command)
-	:key (lambda (item) (or (option-short-name item) #\Nul))
-	:test #'char=))
+        :key (lambda (item) (or (option-short-name item) #\Nul))
+        :test #'char=))
 
 (defmethod find-option ((kind (eql :long)) (command command) name &key)
   "Finds the option with the given long name"
@@ -317,13 +320,13 @@
     (let* ((s-name (option-short-name option))
            (l-name (option-long-name option))
            (s-duplicates (and s-name (remove-if-not
-				      (lambda (x)
-					(char= s-name (option-short-name x)))
-				      remaining)))
+                                      (lambda (x)
+                                        (char= s-name (option-short-name x)))
+                                      remaining)))
            (l-duplicates (and l-name (remove-if-not
-				      (lambda (x)
-					(string= l-name (option-long-name x)))
-				      remaining))))
+                                      (lambda (x)
+                                        (string= l-name (option-long-name x)))
+                                      remaining))))
       (when (> (length s-duplicates) 0)
         (error 'duplicate-options :kind :short :name s-name :items (cons option s-duplicates)))
       (when (> (length l-duplicates) 0)
@@ -334,13 +337,13 @@
   "Ensure that the given COMMAND does not contain duplicate sub-command names"
   (loop :for (sub-command . remaining) :on (command-sub-commands command) :while sub-command :do
     (let* ((sub-names (cons (command-name sub-command)
-			    (command-aliases sub-command)))
+                            (command-aliases sub-command)))
            (duplicates (remove-if-not
-			(lambda (other)
-			  (let ((other-names (cons (command-name other)
-						   (command-aliases other))))
-			    (intersection sub-names other-names :test #'string=)))
-			remaining)))
+                        (lambda (other)
+                          (let ((other-names (cons (command-name other)
+                                                   (command-aliases other))))
+                            (intersection sub-names other-names :test #'string=)))
+                        remaining)))
       (when (> (length duplicates) 0)
         (error 'duplicate-commands :items (cons sub-command duplicates)))))
   t)
@@ -361,9 +364,9 @@
 (defmethod find-sub-command ((command command) name &key)
   "Returns the sub-command with the given name or alias"
   (find-if (lambda (sub-command)
-	     (or (string= name (command-name sub-command))
-		 (member name (command-aliases sub-command) :test #'string=)))
-	   (command-sub-commands command)))
+             (or (string= name (command-name sub-command))
+                 (member name (command-aliases sub-command) :test #'string=)))
+           (command-sub-commands command)))
 
 (defmethod command-full-path ((command command))
   "Returns the full path to the command"
@@ -384,7 +387,7 @@
   (let ((tree (gensym)))
     `(let ((,tree (command-tree ,top-level)))
        (dolist (,node ,tree)
-	 ,@body))))
+         ,@body))))
 
 (defmethod validate-top-level-command ((top-level command))
   "Validates the top-level command and it's sub-commands"
@@ -545,27 +548,27 @@
   "Runs the specified top-level command"
   (handler-case
       (let* ((arguments (or arguments (argv)))
-	     (cmd (parse-command-line top-level arguments)))
-	(unless (command-handler cmd)
-	  (error "No handler registered for command '~A'~%" (command-full-name cmd)))
-	(funcall (command-handler cmd) cmd)
-	(exit 0))
+             (cmd (parse-command-line top-level arguments)))
+        (unless (command-handler cmd)
+          (error "No handler registered for command '~A'~%" (command-full-name cmd)))
+        (funcall (command-handler cmd) cmd)
+        (exit 0))
     ;; Missing required options
     (missing-required-option-value (condition)
       (let ((option (missing-required-option-value-item condition))
-	    (failed-cmd (missing-required-option-value-command condition)))
-	(format *error-output* "Required option ~A not set.~&See '~A --help' for more details.~&"
-		(string-trim #(#\Space) (option-usage-details :default option))
-		(command-full-name failed-cmd))
-	(exit 64))) ;; EX_USAGE
+            (failed-cmd (missing-required-option-value-command condition)))
+        (format *error-output* "Required option ~A not set.~&See '~A --help' for more details.~&"
+                (string-trim #(#\Space) (option-usage-details :default option))
+                (command-full-name failed-cmd))
+        (exit 64))) ;; EX_USAGE
     ;; Missing argument for an option
     (missing-option-argument (condition)
       (let ((option (missing-option-argument-item condition))
-	    (failed-cmd (missing-option-argument-command condition)))
-	(format *error-output* "No value specified for ~A option.~&See '~A --help' for more details.~&"
-		(string-trim #(#\Space) (option-usage-details :default option))
-		(command-full-name failed-cmd))
-	(exit 64))) ;; EX_USAGE
+            (failed-cmd (missing-option-argument-command condition)))
+        (format *error-output* "No value specified for ~A option.~&See '~A --help' for more details.~&"
+                (string-trim #(#\Space) (option-usage-details :default option))
+                (command-full-name failed-cmd))
+        (exit 64))) ;; EX_USAGE
     (error (condition)
       (format *error-output* "~A~&" condition)
       (exit 1))))
@@ -665,6 +668,10 @@
     (declare (ignore value))
     is-set-p))
 
+(defmethod visible-options ((command command))
+  "Returns the list of visible options for the given command"
+  (remove-if #'option-hidden-p (command-options command)))
+
 (defun treat-as-argument (condition)
   "A handler which can be used to invoke the `treat-as-argument' restart"
   (invoke-restart (find-restart 'treat-as-argument condition)))
@@ -676,63 +683,66 @@
 (defmethod print-options-usage ((command command) stream &key (wrap-at-width 70))
   "Prints the usage information about the options for the given command"
   (let* ((opts (sort (copy-list (visible-options command))
-		     #'string<
-		     :key (lambda (x) (option-description-details :default x))))
-	 (usages (mapcar (lambda (o) (option-usage-details :default o)) opts))
-	 (width (+ 4 (apply #'max (mapcar #'length usages)))))
+                     #'string<
+                     :key (lambda (x) (option-description-details :default x))))
+         (usages (mapcar (lambda (o) (option-usage-details :default o)) opts))
+         (width (+ 4 (apply #'max (mapcar #'length usages)))))
     (loop :for (opt usage) :in (mapcar #'list opts usages) :do
       (format stream "  ~A" usage)
       (let* ((desc (option-description-details :default opt))
-	     (lines (split-sequence #\Newline (bobbin:wrap desc wrap-at-width))))
-	(format stream "~vA~A~&" (- width (length usage) 2) #\Space (first lines))
-	(dolist (remaining (rest lines))
-	  (format stream "~vA~A~&" width #\Space remaining)))))
+             (lines (split-sequence #\Newline (bobbin:wrap desc wrap-at-width))))
+        (format stream "~vA~A~&" (- width (length usage) 2) #\Space (first lines))
+        (dolist (remaining (rest lines))
+          (format stream "~vA~A~&" width #\Space remaining)))))
   (format stream "~%"))
 
-(defmethod print-sub-commands-usage ((command command) stream &key (wrap-at-width 70))
+(defmethod print-sub-commands-info ((command command) stream &key (wrap-at-width 70))
   "Prints a summary of the sub-commands available for the command"
   (let* ((sub-commands (command-sub-commands command))
-	 (names-with-aliases (mapcar (lambda (x)
-				       (cons (command-name x) (command-aliases x)))
-				     sub-commands))
-	 (names (mapcar (lambda (x) (join-list x ", ")) names-with-aliases))
-	 (descriptions (mapcar #'command-description sub-commands))
-	 (width (+ 4 (apply #'max (mapcar #'length names)))))
+         (names-with-aliases (mapcar (lambda (x)
+                                       (cons (command-name x) (command-aliases x)))
+                                     sub-commands))
+         (names (mapcar (lambda (x) (join-list x ", ")) names-with-aliases))
+         (descriptions (mapcar #'command-description sub-commands))
+         (width (+ 4 (apply #'max (mapcar #'length names)))))
     (loop :for (name desc) :in (mapcar #'list names descriptions) :do
       (let ((lines (split-sequence #\Newline (bobbin:wrap desc wrap-at-width))))
-	(format stream "  ~A" name)
-	(format stream "~vA~A~&" (- width (length name) 2) #\Space (first lines))
-	(dolist (remaining (rest lines))
-	  (format stream "~vA~A~&" width #\Space remaining)))))
+        (format stream "  ~A" name)
+        (format stream "~vA~A~&" (- width (length name) 2) #\Space (first lines))
+        (dolist (remaining (rest lines))
+          (format stream "~vA~A~&" width #\Space remaining)))))
   (format stream "~%"))
 
+(defmethod command-usage-string ((command command))
+  "Returns the usage string for the given command"
+  (cond
+    ;; The command provides it's own usage info
+    ((command-usage command)
+     (format nil "~A ~A" (command-full-name command) (command-usage command)))
+    ;; The command does not provide it's own usage info and has sub-commands
+    ((command-sub-commands command)
+     (format nil "~A [global-options] command [command-options] [arguments ...]"
+             (command-full-name command)))
+    ;; The command is a sub-command of another command
+    ((command-parent command)
+     (format nil "~A [global options] [command-options] [arguments ...]"
+             (command-full-name command)))
+    ;; Default usage info
+    (t
+     (format nil "~A [options] [arguments ...]" (command-full-name command)))))
 
 (defmethod print-usage ((command command) stream &key (wrap-at 70))
   (format stream "NAME:~%")
   (format stream "  ~A - ~A~2%" (command-full-name command) (command-description command))
 
   (format stream "USAGE:~%")
-  (cond
-    ;; The command provides it's own usage info
-    ((command-usage command)
-     (format stream "  ~A ~A~2%" (command-full-name command) (command-usage command)))
-    ;; The command does not provide it's own usage info and has sub-commands
-    ((command-sub-commands command)
-     (format stream "  ~A [global-options] command [command-options] [arguments ...]~2%"
-	     (command-full-name command)))
-    ;; Command has is a sub-command
-    ((command-parent command)
-     (format stream "  ~A [global options] [command-options] [arguments ...]~2%"
-	     (command-full-name command)))
-    ;; Default usage info
-    (t
-     (format stream "  ~A [options] [arguments ...]~2%" (command-full-name command))))
+  (format stream "  ~A~2%" (command-usage-string command))
 
   (when (command-long-description command)
     (let ((lines (split-sequence #\Newline
-				 (bobbin:wrap (command-long-description command) wrap-at))))
+                                 (bobbin:wrap (command-long-description command) wrap-at))))
       (dolist (line lines)
-	(format stream "  ~A~%" line)))
+        (format stream "  ~A~%" line)))
     (format stream "~%"))
 
   (when (command-options command)
@@ -741,19 +751,19 @@
 
   (when (command-sub-commands command)
     (format stream "COMMANDS:~%")
-    (print-sub-commands-usage command stream))
+    (print-sub-commands-info command stream))
 
   (when (command-examples command)
     (format stream "EXAMPLES:~2%")
     (dolist (example (command-examples command))
       (let* ((description (car example))
-	     (code (cdr example))
-	     (lines (split-sequence #\Newline (bobbin:wrap description wrap-at))))
-	(dolist (line lines)
-	  (format stream "  ~A~%" line))
-	(format stream "~%")
-	(format stream "    ~A~%" code)
-	(format stream "~%"))))
+             (code (cdr example))
+             (lines (split-sequence #\Newline (bobbin:wrap description wrap-at))))
+        (dolist (line lines)
+          (format stream "  ~A~%" line))
+        (format stream "~%")
+        (format stream "    ~A~%" code)
+        (format stream "~%"))))
 
   (when (command-authors command)
     (format stream "AUTHORS:~%")
@@ -765,7 +775,7 @@
     (format stream "LICENSE:~%")
     (let ((lines (split-sequence #\Newline (bobbin:wrap (command-license command) wrap-at))))
       (dolist (line lines)
-	(format stream "  ~A~%" line)))
+        (format stream "  ~A~%" line)))
     (format stream "~%")))
 
 (defmethod print-usage-and-exit ((command command) stream)
@@ -775,9 +785,9 @@
 (defmethod print-version-and-exit ((command command) stream)
   (when (command-version command)
     (format stream
-	    "~A version ~A~&"
-	    (command-full-name command)
-	    (command-version command)))
+            "~A version ~A~&"
+            (command-full-name command)
+            (command-version command)))
   (exit 0))
 
 (defmethod print-bash-completions ((command command) stream)
@@ -800,6 +810,67 @@
   (print-bash-completions command stream)
   (exit 0))
 
-(defmethod visible-options ((command command))
-  "Returns the list of visible options for the given command"
-  (remove-if #'option-hidden-p (command-options command)))
+(defmethod print-documentation ((kind (eql :markdown)) (top-level command) stream &key (wrap-at 80))
+  "Prints the documentation for the given TOP-LEVEL command in Markdown format"
+  (with-command-tree (node top-level)
+    ;; Command name
+    (format stream "# ~A~2%" (command-full-name node))
+
+    ;; Print description
+    (cond
+      ;; Print long description
+      ((command-long-description node)
+       (let ((lines (split-sequence #\Newline
+                                    (bobbin:wrap (command-long-description node) wrap-at))))
+         (dolist (line  lines)
+           (format stream "~A~%" line))
+         (format stream "~%")))
+      ;; Print short description only
+      (t (format stream "`~A` -- ~A~2%" (command-full-name node) (command-description node))))
+
+    ;; Usage info
+    (format stream "## Usage~2%")
+    (format stream "``` shell~%~A~%```~2%" (command-usage-string node))
+
+    ;; Options
+    (when (command-options node)
+      (format stream "## Options~2%")
+      (format stream "`~A` accepts the following options:~2%" (command-full-name node))
+      (format stream "``` shell~%")
+      (print-options-usage node stream)
+      (format stream "```~2%"))
+
+    ;; Sub-commands
+    (when (command-sub-commands node)
+      (format stream "## Sub Commands~2%")
+      (format stream "`~A` provides the following sub commands:~2%" (command-full-name node))
+      (format stream "``` shell~%")
+      (print-sub-commands-info node stream)
+      (format stream "```~2%"))
+
+    ;; Examples
+    (when (command-examples node)
+      (format stream "## Examples~2%")
+      (dolist (example (command-examples node))
+        (let* ((description (car example))
+               (code (cdr example))
+               (lines (split-sequence #\Newline (bobbin:wrap description wrap-at))))
+          (dolist (line lines)
+            (format stream "~A~%" line))
+          (format stream "~%")
+          (format stream "``` shell~%~A~%```~2%" code))))
+
+    ;; Authors
+    (when (command-authors node)
+      (format stream "## Authors~2%")
+      (dolist (author (command-authors node))
+        (format stream "* ~A~%" author))
+      (format stream "~%"))
+
+    ;; License
+    (when (command-license node)
+      (format stream "## License~2%")
+      (let ((lines (split-sequence #\Newline (bobbin:wrap (command-license node) wrap-at))))
+        (dolist (line lines)
+          (format stream "~A~%" line)))
+      (format stream "~%"))))
