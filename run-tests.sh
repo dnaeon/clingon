3a4aa98ce8d9c754709b0cd2d30fa611f45a2383
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-03-24 10:38:19 +0200

    Exit with the code based on the number of failed tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,6 @@ set -e
 LISP=${LISP:-sbcl}
 
 ${LISP} --eval '(ql:quickload :clingon.test)' \
-	--eval '(setf rove:*enable-colors* nil)' \
-	--eval '(asdf:test-system :clingon.test)' \
-	--eval '(quit)'
+        --eval '(setf rove:*enable-colors* nil)' \
+        --eval '(asdf:test-system :clingon.test)' \
+        --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
